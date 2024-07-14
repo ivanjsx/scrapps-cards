594c4eb927a9e29e47a8cc5ef60ab58740f2f3c2
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-14 21:16:28 +0300

    configure app internal state

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,44 @@
 // libraries
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+// components
+import AddCardButton from "../add-card-button/add-card-button";
 
 // styles
 import styles from "./app.module.css";
 
+// types
+import { CardType } from "../../utils/types";
+
+// fake api
+import getInitialCards from "../../utils/fake-api";
+
 
 
 const App: FC = () => {
-  return (
-    <div className={styles.app}></div>
+  
+  const [cards, setCards] = useState<Array<CardType>>([]);
+  
+  useEffect(
+    () => {
+      const fetchData = async (): Promise<void> => {
+        const initialCards = await getInitialCards();
+        setCards(initialCards);
+      };
+      fetchData();
+    }, 
+    []
   );
-}
+  
+  const onAdd = (): void => {};
+  
+  return (
+    <div className={styles.app}>
+      <div className={styles.buttonArea}>
+        <AddCardButton onAdd={onAdd} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
