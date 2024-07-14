573d8bc6de043e82fde3cae6bd6b7ec0b4a628d1
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-14 19:09:40 +0300

    create mocked initial data

---
+++ src/utils/initial-cards.ts
@@ -0,0 +1,24 @@
+import { CardType } from "./types";
+
+
+
+const initialCards: Array<CardType> = [
+  {
+    id: 1,
+    hexColor: "#EEE697",
+  },
+  {
+    id: 2,
+    hexColor: "#D4C0D6",
+  },
+  {
+    id: 3,
+    hexColor: "#F1DFB8",
+  },
+  {
+    id: 4,
+    hexColor: "#808080",
+  },
+];
+
+export default initialCards;
