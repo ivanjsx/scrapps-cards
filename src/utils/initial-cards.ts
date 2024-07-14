88a5762f470c00d92a981f8bd8d0290ca96bde58
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-14 23:25:26 +0300

    change initial color names

--- src/utils/initial-cards.ts
+++ src/utils/initial-cards.ts
@@ -5,19 +5,19 @@ import { CardType } from "./types";
 const initialCards: Array<CardType> = [
   {
     id: 1,
-    hexColor: "#EEE697",
+    hexColor: "tan",
   },
   {
     id: 2,
-    hexColor: "#D4C0D6",
+    hexColor: "khaki",
   },
   {
     id: 3,
-    hexColor: "#F1DFB8",
+    hexColor: "thistle",
   },
   {
     id: 4,
-    hexColor: "#808080",
+    hexColor: "wheat",
   },
 ];
 
